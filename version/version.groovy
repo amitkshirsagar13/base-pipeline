4019d93a4e07adedeb4cd716ffe5a9e6975e4af2
Amit Kshirsagar <amitkshirsagar13@users.noreply.github.com>
2018-11-23 14:19:55 +0530

    Update version.groovy

--- version/version.groovy
+++ version/version.groovy
@@ -8,3 +8,22 @@ def version(version) {
     builder = null
 }
 
+def require(moduleName) {
+    def branch = "master"
+    if ( "${env['flag-feature-toggling']}" == "yes" ) {
+        if ( env['flag-feature-toggling-branch'] != null ) {
+            branch = "${env['flag-feature-toggling-branch']}"
+        }
+    }
+
+    container('maven') {
+        def url = "https://raw.githubusercontent.com/amitkshirsagar13/base-pipeline/${branch}/${moduleName}.groovy"
+        sh """#!/bin/sh -e
+        curl -s -o ./pipeline.base.groovy "${url}"
+        """
+        def func = load("./pipeline.base.groovy")
+        return func
+    }
+}
+
+return this;
